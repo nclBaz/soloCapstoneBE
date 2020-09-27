4f905c4ea84524ee3766774d1b5e39929c138caa
alekszone <50597224+alekszone@users.noreply.github.com>
2020-09-27 21:50:42 +0200

    skill schema

---
+++ server/routes/routes/workes/skills/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const schema = require("./schema")
+const q2m = require("query-to-mongo")
+const {User} = require("../midlewares/middleware")
+
+
+
+
+
+
+
+
+
+
+
---
+++ server/routes/routes/workes/skills/schema.js
@@ -0,0 +1,22 @@
+const {Schema}  =  require("mongoose")
+const mongoose = require("mongoose")
+
+const schema = new Schema(
+    {
+skillName:{
+type:String,
+required:true,
+},
+description:{
+type:String,
+required:true,
+minlength:[50,"Text should be at least 50 character long"]
+},
+profileId:{
+type:String,
+}
+}
+)
+
+const Model = mongoose.model("education", schema)
+module.exports= Model
