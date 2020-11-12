9521ff1dd0a01aeb3e39688b054e1ca86e794b5f
alekszone <50597224+alekszone@users.noreply.github.com>
2020-11-12 12:37:53 +0100

    schema update

--- server/routes/routes/workes/profile/schema.js
+++ server/routes/routes/workes/profile/schema.js
@@ -1,119 +1,124 @@
-const {Schema}= require("mongoose")
-const mongoose = require("mongoose")
-const validator = require("validator")
-const bcrypt = require("bcryptjs")
+const { Schema } = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-
-const profile= new Schema(
-    {
-username:{
-      type:String,
-      required:true,
-      }, 
-  position:{
-type:String,
-required:true,
-  },     
-name:{
-    type:String,
-    required:true,
-},
-surname:{
-    type:String,
-    required:true
-},
-aboutMe:{
-type:String,
-default:"nothing to show"
-},
-dateOfBirth:{
-type:Date,
-},
-email:{
-    type:String,
-    required:true,
-    validate:{
-        validator:async(value)=>{
-            if(!validator.isEmail(value)){
-                throw new Error("Email is invalid")
-            }else{
-                const uniqueEmail = await Model.findOne({email:value})
-                if(uniqueEmail){
-                    throw new Error ("Email already exist")
-                }
+const profile = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    portfolioLink: {
+      type: String,
+    },
+    aboutMe: {
+      type: String,
+      default: "nothing to show",
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: async (value) => {
+          if (!validator.isEmail(value)) {
+            throw new Error("Email is invalid");
+          } else {
+            const uniqueEmail = await Model.findOne({ email: value });
+            if (uniqueEmail) {
+              throw new Error("Email already exist");
             }
-        }
-        }
-},
-password:{
-type:String,
-required:true
-},
+          }
+        },
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
 
+    location: {
+      type: String,
+      required: true,
+    },
+    workExperience: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "workExperience",
+      },
+    ],
+    education: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "education",
+      },
+    ],
+    skills: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "skills",
+      },
+    ],
+    image: {
+      type: String,
+    },
+    token: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+profile.methods.toJSON = function () {
+  let object = this.toObject();
+  delete object.password, delete object.token, delete object.__v;
 
-location:{
-    type:String,
-    required:true
-},
-workExperience:[{
-type:Schema.Types.ObjectId, ref:'workExperience'
-}],
-education:[{
-    type:Schema.Types.ObjectId, ref:'education'
-}],
-skills:[{
-type:Schema.Types.ObjectId, ref:'skills'
-}],
-image:{
-type:String
-},
-token:{
-    type:String
-}
-},
-{timestamps:true}
-)
-profile.methods.toJSON = function(){
-    let object = this.toObject()
-    delete object.password,
-    delete object.token,
-    delete object.__v
-    
-    return object
-    }
-    
+  return object;
+};
 
-profile.statics.findByCredentials = async(email,password)=>{
+profile.statics.findByCredentials = async (email, password) => {
+  const user = await Model.findOne({ email: email });
+  if (!user) {
+    const err = new Error("Email is not Correct ");
+    err.httpStatusCode = 404;
+    throw err;
+  }
 
-    const user = await Model.findOne({email:email})
-  if(!user){
-        const err = new Error ("Email is not Correct ")
-        err.httpStatusCode = 404
-        throw err
-    }
-    
-    const match = await bcrypt.compare(password , user.password)
-   console.log(match)
-   console.log(password)
-   console.log(user.password)
-    if(!match){
-        const err =  new Error ("Password is not Correct");
-        err.httpStatusCode=401
-        throw err
-    }else{
-        return user
-    }
-    }
-    
-profile.pre('save', async function (next) {
-        const user = this;
-        console.log(user)
-        if (user.isModified("password")) {
-          user.password = await bcrypt.hash(user.password, 10);
-        }
-        next();
-      });
-    
- const Model = mongoose.model("workersprofile",profile )
-  module.exports = Model
-      
+  const match = await bcrypt.compare(password, user.password);
+  console.log(match);
+  console.log(password);
+  console.log(user.password);
+  if (!match) {
+    const err = new Error("Password is not Correct");
+    err.httpStatusCode = 401;
+    throw err;
+  } else {
+    return user;
+  }
+};
+
+profile.pre("save", async function (next) {
+  const user = this;
+  console.log(user);
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
+});
+
+const Model = mongoose.model("workersprofile", profile);
+module.exports = Model;
